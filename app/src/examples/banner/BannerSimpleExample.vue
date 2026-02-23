e9645db367a3b3266103b059e7f2c03f9d51101c
Ilya Serdyukov <ilyaserdyukovmmm@gmail.com>
2026-02-24 03:47:09 +1100

    docs: banner (#43)

---
+++ app/src/examples/banner/BannerSimpleExample.vue
@@ -0,0 +1,12 @@
+<template>
+    <Banner>
+        Banner
+        <BannerClose>
+            <i class="fas fa-times"/>
+        </BannerClose>
+    </Banner>
+</template>
+
+<script setup lang="ts">
+import { Banner, BannerClose } from 'webasystvue'
+</script>
