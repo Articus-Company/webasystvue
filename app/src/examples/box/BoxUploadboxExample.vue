3e6b9e7911a6637d70ea84d3c88411116bc0f8c1
Xalindie <ilyaserdyukovmmm@gmail.com>
2026-02-26 22:38:58 +1100

    docs: box

---
+++ app/src/examples/box/BoxUploadboxExample.vue
@@ -0,0 +1,12 @@
+<template>
+    <Box
+        style="width: 300px"
+        uploadbox
+    >
+        Drop files here
+    </Box>
+</template>
+
+<script setup lang="ts">
+import { Box } from 'webasystvue'
+</script>
---
+++ app/src/examples/box/index.ts
@@ -0,0 +1,4 @@
+export { default as BoxAnatomyExample } from './BoxAnatomyExample.vue'
+export { default as BoxContentboxExample } from './BoxContentboxExample.vue'
+export { default as BoxRoundedExample } from './BoxRoundedExample.vue'
+export { default as BoxUploadboxExample } from './BoxUploadboxExample.vue'
