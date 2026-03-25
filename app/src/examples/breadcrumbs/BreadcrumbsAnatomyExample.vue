ec5dbd2a05300889f906a257861f1a5776d7cb41
Ilya Serdyukov <ilyaserdyukovmmm@gmail.com>
2026-03-25 17:02:04 +1100

    docs: breadcrumbs (#51)

---
+++ app/src/examples/breadcrumbs/BreadcrumbsAnatomyExample.vue
@@ -0,0 +1,11 @@
+<template>
+    <Breadcrumbs>
+        <BreadcrumbsSection>
+            <BreadcrumbsItem/>
+        </BreadcrumbsSection>
+    </Breadcrumbs>
+</template>
+
+<script setup lang="ts">
+import { Breadcrumbs, BreadcrumbsItem, BreadcrumbsSection } from 'webasystvue'
+</script>
--- app/src/pages/docs/BreadcrumbsPage.vue
+++ app/src/pages/docs/BreadcrumbsPage.vue
@@ -1,8 +1,77 @@
 <template>
     <DocsLayout>
-        <Article wider>
+        <Article>
             <ArticleBody>
-                <h1>Breadcrumbs</h1>
+                <DocHeading
+                    id="breadcrumbs"
+                    tag="h1"
+                >
+                    Breadcrumbs
+                </DocHeading>
+
+                <DemoCard :code="breadcrumbsDemoExampleRaw">
+                    <BreadcrumbsDemoExample/>
+                </DemoCard>
+
+                <DocHeading
+                    id="anatomy"
+                    tag="h2"
+                    parent="breadcrumbs"
+                >
+                    Анатомия
+                </DocHeading>
+
+                <CodeVueHighlighter :code="breadcrumbsAnatomyExampleRaw"/>
+
+                <DocHeading
+                    id="api"
+                    tag="h2"
+                    parent="breadcrumbs"
+                >
+                    Описание API
+                </DocHeading>
+
+                <DocHeading
+                    id="el-breadcrumbs"
+                    tag="h3"
+                    parent="api"
+                >
+                    &lt;Breadcrumbs&gt;
+                </DocHeading>
+
+                <p>Корневой элемент. Не имеет собственного API. Всегда рендерит <code>ul</code>.</p>
+
+                <DocHeading
+                    id="el-breadcrumbs-section"
+                    tag="h3"
+                    parent="api"
+                >
+                    &lt;BreadcrumbsSection&gt;
+                </DocHeading>
+
+                <ApiPropsTable :rows="[{ prop: 'active', type: 'boolean' }]"/>
+
+                <DocHeading
+                    id="el-breadcrumbs-item"
+                    tag="h3"
+                    parent="api"
+                >
+                    &lt;BreadcrumbsItem&gt;
+                </DocHeading>
+
+                <ApiPropsTable
+                    :rows="[
+                        { prop: 'as', default: `'a'` },
+                        { prop: 'asChild' },
+                    ]"
+                >
+                    <template #type-0>
+                        <ApiPropsAsDesc/>
+                    </template>
+                    <template #type-1>
+                        <ApiPropsAsChildDesc/>
+                    </template>
+                </ApiPropsTable>
             </ArticleBody>
         </Article>
     </DocsLayout>
@@ -10,5 +79,16 @@
 
 <script setup lang="ts">
 import { Article, ArticleBody } from 'webasystvue'
+import { CodeVueHighlighter } from '@/components/code-highlighter'
+import {
+    ApiPropsAsChildDesc,
+    ApiPropsAsDesc,
+    ApiPropsTable,
+} from '@/components/docs/api-props-table'
+import { DemoCard } from '@/components/docs/demo-card'
+import { DocHeading } from '@/components/docs/doc-heading'
+import { BreadcrumbsDemoExample } from '@/examples/breadcrumbs'
+import breadcrumbsAnatomyExampleRaw from '@/examples/breadcrumbs/BreadcrumbsAnatomyExample.vue?raw'
+import breadcrumbsDemoExampleRaw from '@/examples/breadcrumbs/BreadcrumbsDemoExample.vue?raw'
 import { DocsLayout } from '@/layouts'
 </script>
