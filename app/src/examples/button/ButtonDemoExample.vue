1c92b28b1d78bf78f0cf899ba8bdf814225ec682
Ilya Serdyukov <ilyaserdyukovmmm@gmail.com>
2026-03-26 02:32:47 +1100

    docs: button (#53)

---
+++ app/src/examples/button/ButtonDemoExample.vue
@@ -0,0 +1,13 @@
+<template>
+    <Button>Button</Button>
+    <Button
+        circle
+        outlined
+    >
+        <i class="fas fa-heart"/>
+    </Button>
+</template>
+
+<script setup lang="ts">
+import { Button } from 'webasystvue'
+</script>
---
+++ app/src/examples/button/index.ts
@@ -0,0 +1,2 @@
+export { default as ButtonAnatomyExample } from './ButtonAnatomyExample.vue'
+export { default as ButtonDemoExample } from './ButtonDemoExample.vue'
