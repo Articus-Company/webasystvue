a7c46b9cf9eb9607610f202fda561221d51be54e
Xalindie <ilyaserdyukovmmm@gmail.com>
2026-03-25 16:57:25 +1100

    docs: breadcrumbs

---
+++ app/src/examples/breadcrumbs/BreadcrumbsDemoExample.vue
@@ -0,0 +1,28 @@
+<template>
+    <Breadcrumbs>
+        <BreadcrumbsSection>
+            <BreadcrumbsItem>
+                Корневой раздел
+            </BreadcrumbsItem>
+        </BreadcrumbsSection>
+        <BreadcrumbsSection>
+            <BreadcrumbsItem>
+                Каталог
+            </BreadcrumbsItem>
+        </BreadcrumbsSection>
+        <BreadcrumbsSection>
+            <BreadcrumbsItem>
+                Директория
+            </BreadcrumbsItem>
+        </BreadcrumbsSection>
+        <BreadcrumbsSection active>
+            <BreadcrumbsItem>
+                Активный раздел
+            </BreadcrumbsItem>
+        </BreadcrumbsSection>
+    </Breadcrumbs>
+</template>
+
+<script setup lang="ts">
+import { Breadcrumbs, BreadcrumbsItem, BreadcrumbsSection } from 'webasystvue'
+</script>
---
+++ app/src/examples/breadcrumbs/index.ts
@@ -0,0 +1,1 @@
+export { default as BreadcrumbsDemoExample } from './BreadcrumbsDemoExample.vue'
