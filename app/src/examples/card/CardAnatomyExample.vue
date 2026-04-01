92f1e0c88bfd4605dac08b09a31d873b256562f7
Xalindie <ilyaserdyukovmmm@gmail.com>
2026-04-01 22:15:27 +1100

    docs: card

---
+++ app/src/examples/card/CardAnatomyExample.vue
@@ -0,0 +1,10 @@
+<template>
+    <Card>
+        <CardImage/>
+        <CardDetails/>
+    </Card>
+</template>
+
+<script setup lang="ts">
+import { Card, CardDetails, CardImage } from 'webasystvue'
+</script>
---
+++ app/src/examples/card/CardWithTopAndBottomDetails.vue
@@ -0,0 +1,25 @@
+<template>
+    <Card style="width: 300px;">
+        <CardDetails>
+            <h3>Top content</h3>
+        </CardDetails>
+        <CardImage>
+            <img
+                src="https://img.freepik.com/free-photo/long-french-baguette-with-crispy-golden-crust-isolated-beige-studio-background-freshly-baked-type-bread-tasty-nutrition-overhead-shot-tasty-gourmet-product-baked-bakery-food-concept_273609-37921.jpg"
+                alt="baguette"
+            >
+        </CardImage>
+        <CardDetails>
+            <h1>Card Title</h1>
+            <p>Card desc</p>
+        </CardDetails>
+    </Card>
+</template>
+
+<script setup lang="ts">
+import {
+    Card,
+    CardDetails,
+    CardImage,
+} from 'webasystvue'
+</script>
