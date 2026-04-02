50d75373725acc334b8658b0c4795d6d82371aea
Xalindie <ilyaserdyukovmmm@gmail.com>
2026-04-02 18:44:46 +1100

    feat(breadcrumbs): add asChild prop

--- packages/core/src/breadcrumbs/Breadcrumbs.vue
+++ packages/core/src/breadcrumbs/Breadcrumbs.vue
@@ -1,8 +1,28 @@
 <template>
-    <ul class="breadcrumbs">
+    <Primitive
+        as="ul"
+        :class="clsx('breadcrumbs', props.class)"
+        v-bind="delegatedProps"
+    >
         <slot/>
-    </ul>
+    </Primitive>
 </template>
 
+<script lang="ts">
+import type { PrimitiveProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+
+export interface BreadcrumbsProps extends Omit<PrimitiveProps, 'as'> {
+    class?: HTMLAttributes['class']
+}
+</script>
+
 <script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { clsx } from 'clsx'
+import { Primitive } from 'reka-ui'
+
+const props = defineProps<BreadcrumbsProps>()
+
+const delegatedProps = reactiveOmit(props, 'class')
 </script>
