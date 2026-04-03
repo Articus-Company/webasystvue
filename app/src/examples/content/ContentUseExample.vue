e79f25128aada7cf22c51640a8f1772af36b5e40
Ilya Serdyukov <ilyaserdyukovmmm@gmail.com>
2026-04-03 18:31:41 +1100

    docs: content (#62)

---
+++ app/src/examples/content/ContentUseExample.vue
@@ -0,0 +1,12 @@
+<template>
+    <Flexbox>
+        <Sidebar/>
+        <Content>
+            <RouterView/>
+        </Content>
+    </Flexbox>
+</template>
+
+<script setup lang="ts">
+import { Content, Flexbox, Sidebar } from 'webasystvue'
+</script>
--- app/src/pages/docs/ContentPage.vue
+++ app/src/pages/docs/ContentPage.vue
@@ -1,8 +1,74 @@
 <template>
     <DocsLayout>
-        <Article wider>
+        <Article>
             <ArticleBody>
-                <h1>Content</h1>
+                <DocHeading
+                    id="content"
+                    tag="h1"
+                >
+                    Content
+                </DocHeading>
+
+                <DocHeading
+                    id="anatomy"
+                    tag="h2"
+                    parent="content"
+                >
+                    Анатомия
+                </DocHeading>
+
+                <CodeVueHighlighter :code="contentAnatomyExampleRaw"/>
+
+                <DocHeading
+                    id="api"
+                    tag="h2"
+                    parent="content"
+                >
+                    Описание API
+                </DocHeading>
+
+                <DocHeading
+                    id="el-content"
+                    tag="h3"
+                    parent="api"
+                >
+                    &lt;Content&gt;
+                </DocHeading>
+
+                <ApiPropsTable
+                    :rows="[
+                        { prop: 'as' },
+                        { prop: 'asChild' },
+                    ]"
+                >
+                    <template #type-0>
+                        <ApiPropsAsDesc/>
+                    </template>
+                    <template #type-1>
+                        <ApiPropsAsChildDesc/>
+                    </template>
+                </ApiPropsTable>
+
+                <DocHeading
+                    id="examples"
+                    tag="h2"
+                    parent="content"
+                >
+                    Примеры
+                </DocHeading>
+
+                <DocHeading
+                    id="use-example"
+                    tag="h3"
+                    parent="examples"
+                >
+                    Пример использования компонента
+                </DocHeading>
+
+                <CodeVueHighlighter
+                    :code="contentUseExampleRaw"
+                    highlight-lines="{4,6}"
+                />
             </ArticleBody>
         </Article>
     </DocsLayout>
@@ -10,5 +76,14 @@
 
 <script setup lang="ts">
 import { Article, ArticleBody } from 'webasystvue'
+import { CodeVueHighlighter } from '@/components/code-highlighter'
+import {
+    ApiPropsAsChildDesc,
+    ApiPropsAsDesc,
+    ApiPropsTable,
+} from '@/components/docs/api-props-table'
+import { DocHeading } from '@/components/docs/doc-heading'
+import contentAnatomyExampleRaw from '@/examples/content/ContentAnatomyExample.vue?raw'
+import contentUseExampleRaw from '@/examples/content/ContentUseExample.vue?raw'
 import { DocsLayout } from '@/layouts'
 </script>
