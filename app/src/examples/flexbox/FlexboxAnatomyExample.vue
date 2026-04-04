14d53aa145c3782b14fe68c42a910b73c4f90d01
Ilya Serdyukov <ilyaserdyukovmmm@gmail.com>
2026-04-04 20:28:29 +1100

    docs: flexbox (#63)

---
+++ app/src/examples/flexbox/FlexboxAnatomyExample.vue
@@ -0,0 +1,7 @@
+<template>
+    <Flexbox/>
+</template>
+
+<script setup lang="ts">
+import { Flexbox } from 'webasystvue'
+</script>
---
+++ app/src/examples/flexbox/FlexboxFullWidthExample.vue
@@ -0,0 +1,32 @@
+<template>
+    <div style="width: 500px">
+        <Flexbox
+            style="border: 1px dashed gold"
+            full-width
+        >
+            <span>
+                Элемент 1
+            </span>
+            <div>
+                <Userpic
+                    as="img"
+                    src="/wa-content/img/userpic20.jpg"
+                    alt="userpic"
+                    title="Элемент 2"
+                />
+            </div>
+            <div>
+                <Button>
+                    Элемент 3
+                </Button>
+            </div>
+            <div style="padding: 20px 5px; background: rgba(33,66,88,.2);">
+                Элемент 4
+            </div>
+        </Flexbox>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Button, Flexbox, Userpic } from 'webasystvue'
+</script>
