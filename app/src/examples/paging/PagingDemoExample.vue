623bf1244d6f0fe9c42f07e9fb30472ec2a44d10
Ilya Serdyukov <ilyaserdyukovmmm@gmail.com>
2026-04-27 16:57:02 +1100

    docs: paging (#73)

---
+++ app/src/examples/paging/PagingDemoExample.vue
@@ -0,0 +1,35 @@
+<template>
+    <Paging>
+        <PagingItem>
+            <span>←</span>
+        </PagingItem>
+        <PagingItem>
+            <span>1</span>
+        </PagingItem>
+        <PagingItem>
+            <span>2</span>
+        </PagingItem>
+        <PagingItem>
+            <span>3</span>
+        </PagingItem>
+        <PagingItem selected>
+            <span>4</span>
+        </PagingItem>
+        <PagingItem>
+            <span>5</span>
+        </PagingItem>
+        <PagingItem>
+            <span>...</span>
+        </PagingItem>
+        <PagingItem>
+            <span>21</span>
+        </PagingItem>
+        <PagingItem>
+            <span>→</span>
+        </PagingItem>
+    </Paging>
+</template>
+
+<script setup lang="ts">
+import { Paging, PagingItem } from 'webasystvue'
+</script>
--- app/src/pages/docs/PagingPage.vue
+++ app/src/pages/docs/PagingPage.vue
@@ -1,8 +1,80 @@
 <template>
     <DocsLayout>
-        <Article wider>
+        <Article>
             <ArticleBody>
-                <h1>Paging</h1>
+                <DocHeading
+                    id="paging"
+                    tag="h1"
+                >
+                    Paging
+                </DocHeading>
+
+                <DemoCard :code="pagingDemoExampleRaw">
+                    <PagingDemoExample/>
+                </DemoCard>
+
+                <DocHeading
+                    id="anatomy"
+                    tag="h2"
+                    parent="paging"
+                >
+                    Анатомия
+                </DocHeading>
+
+                <CodeVueHighlighter :code="pagingAnatomyExampleRaw"/>
+
+                <DocHeading
+                    id="api"
+                    tag="h2"
+                    parent="paging"
+                >
+                    Описание API
+                </DocHeading>
+
+                <DocHeading
+                    id="el-paging"
+                    tag="h3"
+                    parent="api"
+                >
+                    &lt;Paging&gt;
+                </DocHeading>
+
+                <ApiPropsTable
+                    :rows="[
+                        { prop: 'as', default: `'ul'` },
+                        { prop: 'asChild' },
+                    ]"
+                >
+                    <template #type-0>
+                        <ApiPropsAsDesc/>
+                    </template>
+                    <template #type-1>
+                        <ApiPropsAsChildDesc/>
+                    </template>
+                </ApiPropsTable>
+
+                <DocHeading
+                    id="el-paging-item"
+                    tag="h3"
+                    parent="api"
+                >
+                    &lt;PagingItem&gt;
+                </DocHeading>
+
+                <ApiPropsTable
+                    :rows="[
+                        { prop: 'as', default: `'li'` },
+                        { prop: 'asChild' },
+                        { prop: 'selected', type: 'boolean' },
+                    ]"
+                >
+                    <template #type-0>
+                        <ApiPropsAsDesc/>
+                    </template>
+                    <template #type-1>
+                        <ApiPropsAsChildDesc/>
+                    </template>
+                </ApiPropsTable>
             </ArticleBody>
         </Article>
     </DocsLayout>
@@ -10,5 +82,16 @@
 
 <script setup lang="ts">
 import { Article, ArticleBody } from 'webasystvue'
+import { CodeVueHighlighter } from '@/components/code-highlighter'
+import {
+    ApiPropsAsChildDesc,
+    ApiPropsAsDesc,
+    ApiPropsTable,
+} from '@/components/docs/api-props-table'
+import { DemoCard } from '@/components/docs/demo-card'
+import { DocHeading } from '@/components/docs/doc-heading'
+import { PagingDemoExample } from '@/examples/paging'
+import pagingAnatomyExampleRaw from '@/examples/paging/PagingAnatomyExample.vue?raw'
+import pagingDemoExampleRaw from '@/examples/paging/PagingDemoExample.vue?raw'
 import { DocsLayout } from '@/layouts'
 </script>
