fef9158a28a4153a05bc75785e5810450d5e3097
Ilya Serdyukov <ilyaserdyukovmmm@gmail.com>
2026-04-30 05:27:13 +1100

    docs: tablebox (#83)

---
+++ app/src/examples/tablebox/TableboxFixedExample.vue
@@ -0,0 +1,14 @@
+<template>
+    <Tablebox
+        style="border: 1px dashed gold; width: 700px"
+        fixed
+    >
+        <div>Я обычная колонка</div>
+        <div>Я колонка в центре и чуть побольше</div>
+        <div>А я колонка справа</div>
+    </Tablebox>
+</template>
+
+<script setup lang="ts">
+import { Tablebox } from 'webasystvue'
+</script>
---
+++ app/src/examples/tablebox/TableboxMiddleExample.vue
@@ -0,0 +1,15 @@
+<template>
+    <Tablebox
+        style="border: 1px dashed gold; width: 700px"
+        fixed
+        middle
+    >
+        <div>Я обычная колонка</div>
+        <div>Я колонка в центре и чуть побольше</div>
+        <div>А я колонка справа</div>
+    </Tablebox>
+</template>
+
+<script setup lang="ts">
+import { Tablebox } from 'webasystvue'
+</script>
