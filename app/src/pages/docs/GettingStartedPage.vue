cbc6046dcd7920d7f53cc2a78160c9b2918df431
Ilya Serdyukov <ilyaserdyukovmmm@gmail.com>
2026-05-01 22:42:45 +1100

    docs: getting started (#94)

--- app/src/pages/docs/GettingStartedPage.vue
+++ app/src/pages/docs/GettingStartedPage.vue
@@ -1,8 +1,59 @@
 <template>
     <DocsLayout>
-        <Article wider>
+        <Article>
             <ArticleBody>
-                <h1>Начало работы</h1>
+                <DocHeading
+                    id="getting-started"
+                    tag="h1"
+                >
+                    Начало работы
+                </DocHeading>
+
+                <DocHeading
+                    id="install"
+                    tag="h2"
+                    parent="getting-started"
+                >
+                    Установка
+                </DocHeading>
+
+                <InstallCommands/>
+
+                <DocHeading
+                    id="usage"
+                    tag="h2"
+                    parent="getting-started"
+                >
+                    Использование
+                </DocHeading>
+
+                <p>
+                    Библиотека предоставляет набор Vue-компонентов, которые полностью типизированы и готовы к использованию.
+                    Просто импортируйте нужный компонент и вставляйте его в шаблон.
+                </p>
+
+                <DocHeading
+                    id="example"
+                    tag="h3"
+                    parent="usage"
+                >
+                    Простой пример
+                </DocHeading>
+
+                <CodeVueHighlighter :code="gettingStartedUsageExampleRaw"/>
+
+                <DocHeading
+                    id="composition"
+                    tag="h3"
+                    parent="usage"
+                >
+                    Композиция компонентов
+                </DocHeading>
+
+                <p>
+                    Библиотека построена на принципе Compound Components.
+                    Это даёт полный контроль над вёрсткой без ограничений фиксированной структуры.
+                </p>
             </ArticleBody>
         </Article>
     </DocsLayout>
@@ -10,5 +61,9 @@
 
 <script setup lang="ts">
 import { Article, ArticleBody } from 'webasystvue'
+import { CodeVueHighlighter } from '@/components/code-highlighter'
+import { DocHeading } from '@/components/docs/doc-heading'
+import { InstallCommands } from '@/components/docs/install-commands'
+import gettingStartedUsageExampleRaw from '@/examples/getting-started/GettingStartedUsageExample.vue?raw'
 import { DocsLayout } from '@/layouts'
 </script>
